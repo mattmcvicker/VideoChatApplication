b8f741ad58c91ba6613676a0e3d3db3010348d00
mattmcv <23mcvicker@gmail.com>
2020-11-30 00:16:11 -0800

    Add webRTC local connection between users

--- wefeud-react-app/src/App.js
+++ wefeud-react-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import VideoChatContainer from './VideoChatContainer'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>React Video Chat App</h1>
+      <h2>WebRTC + Firebase</h2>
+      <VideoChatContainer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ wefeud-react-app/src/config.js
@@ -0,0 +1,13 @@
+const config = {
+    apiKey: "AIzaSyApgYRfIB68ptyvmzBDYv--hSqhe15va3U",
+    authDomain: "wefeud.firebaseapp.com",
+    databaseURL: "https://wefeud.firebaseio.com",
+    projectId: "wefeud",
+    storageBucket: "wefeud.appspot.com",
+    messagingSenderId: "45972366037",
+    appId: "1:45972366037:web:766daf663af3306ae7785c",
+    measurementId: "G-H24KDFPWJL"
+  };
+
+
+export default config;
